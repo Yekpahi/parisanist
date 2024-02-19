93fc11614443e68a2a636c1450bb52c7387c81a3
Yekpahi Prince <princeyekpahi@gmail.com>
2024-02-19 15:41:45 +0100

    header and footer

--- userauths/views.py
+++ userauths/views.py
@@ -130,7 +130,7 @@ def login(request):
 def logout(request):
     auth.logout(request)
     messages.success(request, "You are logged out.")
-    return redirect('login')
+    return redirect('home')
 
 # Account activation
 
